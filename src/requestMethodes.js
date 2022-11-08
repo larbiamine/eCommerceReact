59d0143437e8a3133f189d5f1f61e8d50635c239
larbiamine <larbiaminelj@gmail.com>
2022-11-08 15:35:14 +0100

    Fixed Token

--- src/requestMethodes.js
+++ src/requestMethodes.js
@@ -8,7 +8,7 @@ const BASE_URL = "http://localhost:5000/api/";
 // const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 // const currentUser = user && JSON.parse(user).currentUser;
 // const TOKEN = currentUser?.accessToken;
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjkxYzFiMDA4Nzk4MWY2NDVkYTA0MCIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2Njc4MzI4NzAsImV4cCI6MTY2ODA5MjA3MH0.sWU8ANhfZH9qiG6EYGiaCS6o2XcIos_4ZkLw8pBDJdw";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
@@ -16,5 +16,5 @@ export const publicRequest = axios.create({
 
 export const userRequest = axios.create({
   baseURL: BASE_URL,
-  header: { token: `Bearer ${TOKEN}` },
+  headers: { token: `Bearer ${TOKEN}` },
 });
