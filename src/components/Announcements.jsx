ceedb76ac77b65ab85b05117012c8f8ad6f21b93
larbiamine <larbiaminelj@gmail.com>
2022-12-26 22:16:57 +0100

    nav bar anoucement

--- src/components/Announcements.jsx
+++ src/components/Announcements.jsx
@@ -1,19 +1,30 @@
-import styled from 'styled-components'
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Container = styled.div`
-    height: 30px;
-    background-color: #9649CB;
-    color: white;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: 14px;
-    font-weight: 500;
-`
+	height: 30px;
+	background-color: #9649cb;
+	color: white;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	font-size: 14px;
+	font-weight: 500;
+`;
+
 function Announcements() {
-  return (
-    <Container>Announcements</Container>
-  )
+	const currentUser = useSelector((state) => state.user.currentUser?.username);
+	return (
+		!currentUser && (
+			<Container>
+				<span>
+					You're not logged in, <Link to="/login">{"Sign in "}</Link> {" Or "}
+					<Link to="/register">Register</Link>
+				</span>
+			</Container>
+		)
+	);
 }
 
-export default Announcements
+export default Announcements;
